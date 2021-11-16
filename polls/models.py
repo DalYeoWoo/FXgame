fe3083842f73f08a14b3ab28788509cbcd24a43e
Dal0328 <dhkh1111@snu.ac.kr>
2021-11-16 11:11:30 +0900

    final?

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Participant(models.Model):
     history = models.CharField(max_length=200, default=':')
+    answer = models.CharField(max_length=200, default=':')
+    hint = models.CharField(max_length=200, default=':')
     tip = models.CharField(max_length=200, default=':')
     group = models.CharField(max_length=1, default=':')
 
--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 import random
-poll_group=['A','B','C','D','E']
+poll_group=['A','B','C','D']
 
 def index(request):
     return render(request, 'polls/index.html')
@@ -45,5 +45,7 @@ def submit(request, participant_id):
     participant = get_object_or_404(Participant, pk=participant_id)
     if(request.method == 'POST'):
         participant.history = str(request.POST.get('hist', ''))
+        participant.answer = str(request.POST.get('ans', ''))
+        participant.hint = str(request.POST.get('info', ''))
         participant.save()
     return redirect(reverse('polls:tip',args=[participant_id]))
